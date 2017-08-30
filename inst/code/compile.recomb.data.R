df369481fbc87c995a0e67b3a4bae5a35c662e29
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-08-30 10:45:48 -0500

    Implemented script compile.recomb.data.R and created a new data set, recomb.m38.RData.

---
+++ inst/code/compile.recomb.data.R
@@ -0,0 +1,10 @@
+# TO DO: Explain here what this script does.
+
+# Load the recombination rate data from Brunschwig et al (2012),
+# "lifted over" to the Mouse Genome Assembly 38 (mm10).
+recomb.m38 <- read.table("../data/brunschwig_tableS1_mm10.txt.gz",sep = " ",
+                         stringsAsFactors = TRUE,header = TRUE)
+recomb.m38 <- transform(recomb.m38,chr = factor(chr))
+
+# Save the data to an .RData file.
+save(list = "recomb.m38", file = "recomb.m38.RData")
